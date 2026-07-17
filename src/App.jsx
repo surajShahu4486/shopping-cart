5e0f7ccdfc365cd8ae50b6e48a9eee22cda96ab4
sshahu8055 <sshahu@valethi.in>
2026-07-17 14:26:54 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,54 @@
-import React from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import { createBrowserRouter,RouterProvider} from 'react-router'
-import Navbar from './Components/Navbar'
-import Productlist from './Pages/Productlist'
-import ProductDetails from './Pages/ProductDetails'
-import HomePage from './Components/HomePage'
-import Cart  from './Pages/Cart'
-import OrderConfirmation from './Pages/OrderConfirmation'
-import Checkout from './Pages/Checkout';
+import Navbar from "./Components/Navbar";
+import Productlist from "./Pages/Productlist";
+import ProductDetails from "./Pages/ProductDetails";
+import HomePage from "./Components/HomePage";
+import Cart from "./Pages/Cart";
+import OrderConfirmation from "./Pages/OrderConfirmation";
+import Checkout from "./Pages/Checkout";
 
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Navbar />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: "productlist",
+          element: <Productlist />,
+        },
+        {
+          path: "productDetails/:id",
+          element: <ProductDetails />,
+        },
+        {
+          path: "cart",
+          element: <Cart />,
+        },
+        {
+          path: "checkout",
+          element: <Checkout />,
+        },
+        {
+          path: "orderConfirmation",
+          element: <OrderConfirmation />,
+        },
+      ],
+    },
+  ],
+  {
+    basename: "/shopping-cart",
+  }
+);
 
-const router = createBrowserRouter([
-  
-  {path:"/",
-    element:<Navbar />,
-    children:[
-      { index: true, element: <HomePage /> },
-      { path:"productlist",element:<Productlist /> },
-      { path:"productDetails/:id", element:<ProductDetails/>},
-      { path:"cart",element:<Cart/>},
-      { path:"Checkout",element:<Checkout/>},
-      {path:"orderConfirmation",element:<OrderConfirmation/>}
-      
-    ]
-
-  },
-])
 const App = () => {
-  return <RouterProvider router={router}></RouterProvider>
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
